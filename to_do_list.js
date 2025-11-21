7bdd7614a2d864e4f2af623c0d96abcdbabb38a0
G. Siva Sai Poojitha <poojithagedela2007@gmail.com>
2025-11-21 21:28:57 +0530

    Add files via upload

---
+++ to_do_list.js
@@ -0,0 +1,13 @@
+function addTask() {
+    let task = document.getElementById("taskInput").value;
+
+    if(task === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    document.getElementById("taskList").appendChild(li);
+
+    document.getElementById("taskInput").value = "";
+}
+
